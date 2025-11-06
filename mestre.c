78d1b8b1839b0b446d0daec70f7bde623012eedc
carlosmpinheiro-79 <carlosmpinheiro@gmail.com>
2025-11-05 21:11:39 -0300

    Add territories and implement attack functionality
    Added territories with names, colors, and troop counts. Implemented attack functionality and displayed updated map state after an attack.

--- mestre.c
+++ mestre.c
@@ -1,1 +1,74 @@
 
+
+Território 1:
+Nome: amurica
+Cor do exército: azul
+Número de tropas: 3
+
+Território 2:
+Nome: rio
+Cor do exército: vermelho
+Número de tropas: 5
+
+Território 3:
+Nome: terra
+Cor do exército: marrom
+Número de tropas: 3
+
+Território 4:
+Nome: mumu
+Cor do exército: amarelo
+Número de tropas: 2
+
+Território 5:
+Nome: bra
+Cor do exército: verde
+Número de tropas: 6
+
+=== Estado Atual do Mapa ===
+Território 1:
+Nome: murica | Cor: azul | Tropas: 3
+
+Território 2:
+Nome: rio | Cor: vermelho | Tropas: 5
+
+Território 3:
+Nome: terra | Cor: marrom | Tropas: 3
+
+Território 4:
+Nome: mumu | Cor: amarelo | Tropas: 2
+
+Território 5:
+Nome: bra | Cor: verde | Tropas: 6
+
+1 - Atacar
+0 - Sair
+Escolha: 1
+Escolha o território atacante (1-5): 4
+Escolha o território defensor (1-5): 5
+
+Ataque de mumu (2 tropas) contra bra (6 tropas)
+Dado atacante: 4 | Dado defensor: 3
+Defensor perdeu 1 tropa!
+
+=== Estado Atual do Mapa ===
+Território 1:
+Nome: murica | Cor: azul | Tropas: 3
+
+Território 2:
+Nome: rio | Cor: vermelho | Tropas: 5
+
+Território 3:
+Nome: terra | Cor: marrom | Tropas: 3
+
+Território 4:
+Nome: mumu | Cor: amarelo | Tropas: 2
+
+Território 5:
+Nome: bra | Cor: verde | Tropas: 5
+
+1 - Atacar
+0 - Sair
+Escolha: 0
+[1] + Done                       "/usr/bin/gdb" --interpreter=mi --tty=${DbgTerm} 0<"/tmp/Microsoft-MIEngine-In-3re4dk2l.014" 1>"/tmp/Microsoft-MIEngine-Out-qdlxjcof.jsv"
+@carlosmpinheiro-79 ➜ /workspaces/war-carlosmpinheiro-79 (main) $ AA
